62fb6814d8d4a15b93a966179ed28b30549c2ea8
Mariia Mieradzhy <maroagogle123@gmail.com>
2026-02-18 11:01:37 +0200

    Update form design

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 document.getElementById("registrationForm").addEventListener("submit", function(event) {
 
-    event.preventDefault(); // забороняємо перезавантаження сторінки
-
+    event.preventDefault(); //забороняємо перезавантаження сторінки
     let isValid = true;
-
-    // Отримуємо значення
     let name = document.getElementById("name").value.trim();
     let email = document.getElementById("email").value.trim();
     let password = document.getElementById("password").value.trim();
@@ -13,7 +10,7 @@ document.getElementById("registrationForm").addEventListener("submit", function(
     // Очищаємо попередні помилки
     document.querySelectorAll(".error").forEach(el => el.textContent = "");
 
-    // 1️⃣ Перевіряєио порожні поля
+    //Перевіряєио порожні поля
     if (name === "") {
         document.getElementById("nameError").textContent = "Поле не може бути порожнім";
         isValid = false;
@@ -30,23 +27,23 @@ document.getElementById("registrationForm").addEventListener("submit", function(
         document.getElementById("confirmError").textContent = "Поле не може бути порожнім";
         isValid = false;
     }
-    // 2️⃣ Перевіряємо email на @
+    //Перевіряємо email на @
     if (email !== "" && !email.includes("@")) {
         document.getElementById("emailError").textContent = "Email має містити символ @";
         isValid = false;
     }
-    // 3️⃣ Перевіряємо довжини пароля
+    //Перевіряємо довжини пароля
     if (password.length > 0 && password.length < 8) {
         document.getElementById("passwordError").textContent = "Пароль має бути не менше 8 символів";
         isValid = false;
     }
 
-    // 4️⃣ Перевірка співпадіння паролів
+    //Перевірка співпадіння паролів
     if (password !== confirmPassword && confirmPassword !== "") {
         document.getElementById("confirmError").textContent = "Невірний пароль. Паролі не співпадають!";
         isValid = false;
     }
-
+    //якщо все успішно
     if (isValid) {
         alert("Реєстрація успішна!");
     }
